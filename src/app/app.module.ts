eaf9d9e169ee542d56b4c3ece2a4946d352d6656
scriptSQD <73490413+scriptSQD@users.noreply.github.com>
2022-02-07 23:26:24 +0300

    [I] - Implement scrollable Gallery. [I] - Add media. [M] - Fix footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SwiperModule } from 'swiper/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { MainComponent } from './main/main.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import SwiperCore, { Navigation, Pagination, Zoom } from 'swiper'
+import * as Rellax from 'rellax';
+
+SwiperCore.use([Pagination, Zoom, Navigation]);
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor() {};
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const rellax = new Rellax('.to-rellax', {
+      speed: 2,
+      center: false,
+      vertical: true,
+      horizontal: false
+    });
   }
 
 }
