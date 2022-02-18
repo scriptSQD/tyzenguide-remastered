698b0863164ba4ceda135857409031e2aa8d07ec
scriptSQD <73490413+scriptSQD@users.noreply.github.com>
2022-02-18 21:53:16 +0300

    [I] - Replace YouTube video links to reflect new project channel.

--- src/app/sights/sight/sight.component.ts
+++ src/app/sights/sight/sight.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, of } from 'rxjs';
 
